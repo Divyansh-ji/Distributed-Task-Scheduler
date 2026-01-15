b0e30f34b335ace75e3872f5f87730dce825892f
Divyansh-ji <Divyanshtiwary01@gmail.com>
2026-01-15 10:58:00 +0530

    imported

--- services/internals/api/handler.go
+++ services/internals/api/handler.go
@@ -1,1 +1,44 @@
 package api
+
+import (
+	"main/DistributedTaskScheduler/services/internals/redis"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+type TaskRequest struct {
+	Payload string `json:"payload" binding:"required"`
+}
+
+type TaskResponse struct {
+	TaskID string `json:"task_id"`
+	Status string `json:"status"`
+}
+
+func CreateTask(rdb *redis.Client) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var req TaskRequest
+		if err := c.BindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "invalid request body",
+			})
+			return
+		}
+		taskID := uuid.New().String()
+
+		err := redis.PushTask(rdb, taskID, req.Payload)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "failed to queue task",
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, TaskResponse{
+			TaskID: taskID,
+			Status: "queued",
+		})
+	}
+}

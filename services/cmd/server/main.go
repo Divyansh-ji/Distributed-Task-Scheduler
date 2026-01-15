411996f4318e7e5abfcc3a1485a9b286ed190b48
Divyansh-ji <Divyanshtiwary01@gmail.com>
2026-01-15 22:50:36 +0530

    df

--- services/cmd/server/main.go
+++ services/cmd/server/main.go
@@ -1,1 +1,17 @@
-package server
+package main
+
+import (
+	"log"
+
+	"DistributedTaskScheduler/services/internals/api"
+	"DistributedTaskScheduler/services/internals/redis"
+)
+
+func main() {
+	rdb := redis.NewRedisClient()
+
+	router := api.RegisterRoutes(rdb)
+
+	log.Println("🚀 Gin API server running on :8080")
+	router.Run(":8080")
+}

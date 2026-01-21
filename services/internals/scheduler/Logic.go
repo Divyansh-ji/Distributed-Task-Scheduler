19145aa3345eacb443069daf4d65dd3b729f38ea
Divyansh-ji <Divyanshtiwary01@gmail.com>
2026-01-21 23:59:24 +0530

    df

--- services/internals/scheduler/Logic.go
+++ services/internals/scheduler/Logic.go
@@ -3,11 +3,27 @@ package scheduler
 import (
 	"context"
 	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type Task struct {
-	Id        int
-	Type      func(context.Context)
-	ExecuteAt time.Time
-	Status    string
+	Id         int
+	Type       func(context.Context)
+	ExecuteAt  time.Time
+	Status     string
+	RetryCount int
+}
+
+func ScheduleTask(
+	ctx context.Context,
+	rdb *redis.Client,
+	taskID int,
+	executeAt time.Time,
+) error {
+
+	return rdb.ZAdd(ctx, "DelayedTasksKey", redis.Z{
+		Score:  float64(executeAt.Unix()),
+		Member: taskID,
+	}).Err()
 }

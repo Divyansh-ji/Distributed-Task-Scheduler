3d40f6043ad57a62ba93d932185ea8be8b2e561e
Divyansh-ji <Divyanshtiwary01@gmail.com>
2026-01-23 20:06:32 +0530

    wired

--- services/cmd/server/main.go
+++ services/cmd/server/main.go
@@ -3,23 +3,44 @@ package main
 import (
 	"context"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"DistributedTaskScheduler/services/internals/api"
 	"DistributedTaskScheduler/services/internals/redis"
 	"DistributedTaskScheduler/services/internals/scheduler"
+	"DistributedTaskScheduler/services/internals/worker"
 )
 
 func main() {
-	//we will create root context
+	// root context
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
+	// redis
 	rdb := redis.NewRedisClient()
-	scheduler := scheduler.NewScheduler(rdb, 500*time.Millisecond)
 
-	go scheduler.Start(ctx)
+	// scheduler
+	s := scheduler.NewScheduler(rdb, 500*time.Millisecond)
+	go s.Start(ctx)
 
+	// worker
+	go worker.Worker(ctx, rdb)
+
+	// api
 	router := api.RegisterRoutes(rdb)
+
+	// graceful shutdown (VERY IMPORTANT)
+	go func() {
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+		<-sigCh
+
+		log.Println("🛑 Shutting down services...")
+		cancel()
+	}()
 
 	log.Println("🚀 Gin API server running on :8080")
 	router.Run(":8080")

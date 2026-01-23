93ac407002c7a1467bbdaf7552fe43979b81c993
Divyansh-ji <Divyanshtiwary01@gmail.com>
2026-01-23 16:28:11 +0530

    the commits

--- services/cmd/server/main.go
+++ services/cmd/server/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"context"
 	"log"
+	"time"
 
 	"DistributedTaskScheduler/services/internals/api"
 	"DistributedTaskScheduler/services/internals/redis"
+	"DistributedTaskScheduler/services/internals/scheduler"
 )
 
 func main() {
+	//we will create root context
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	rdb := redis.NewRedisClient()
+	scheduler := scheduler.NewScheduler(rdb, 500*time.Millisecond)
+
+	go scheduler.Start(ctx)
 
 	router := api.RegisterRoutes(rdb)
 

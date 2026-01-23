6aa307634e69b3d5f4e4cc5d103dd45c9a61ee62
Divyansh-ji <Divyanshtiwary01@gmail.com>
2026-01-23 17:52:01 +0530

    the commit

--- services/internals/worker/worker.go
+++ services/internals/worker/worker.go
@@ -4,46 +4,63 @@ import (
 	"DistributedTaskScheduler/services/internals/api"
 	"DistributedTaskScheduler/services/internals/tasks"
 	"context"
+	"encoding/json"
 	"log"
 
 	"github.com/redis/go-redis/v9"
 )
 
+type Task struct {
+	ID      string
+	Type    string
+	Payload api.TaskRequest
+}
+
 const ReadyQueueKey = "scheduler:ready_queue"
 
-func worker(ctx context.Context, rdb *redis.Client) {
+func Worker(ctx context.Context, rdb *redis.Client) {
 	for {
 		select {
 		case <-ctx.Done():
 			log.Println("Worker stopping")
 			return
+
 		default:
 			result, err := rdb.BRPop(ctx, 0, ReadyQueueKey).Result()
 			if err != nil {
 				log.Println(err)
 				continue
 			}
+
 			if len(result) < 2 {
 				log.Println("Invalid BRPop result:", result)
 				continue
 			}
-			taskID := result[1]
-			go processTask(ctx, taskID)
+
+			taskJSON := result[1]
+
+			var task Task
+			if err := json.Unmarshal([]byte(taskJSON), &task); err != nil {
+				log.Println("Failed to unmarshal task:", err)
+				continue
+			}
+
+			// one task = one execution
+			processTask(ctx, task)
 		}
 	}
 }
-func processTask(ctx context.Context, taskID string) {
-	switch taskID {
+
+func processTask(ctx context.Context, task Task) {
+	switch task.Type {
+
 	case "sendEmail":
-		// Create a dummy api.TaskRequest or fetch the actual task details as needed
-		var req api.TaskRequest
-		go tasks.SendEmail(ctx, req)
+		go tasks.SendEmail(ctx, task.Payload)
 
 	case "generateReport":
-		var reqs api.TaskRequest
+		go tasks.GenerateReport(ctx, task.Payload)
 
-		go tasks.GenerateReport(ctx, reqs)
-
+	default:
+		log.Println("Unknown task type:", task.Type)
 	}
-
 }

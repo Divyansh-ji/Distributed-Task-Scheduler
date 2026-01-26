62ec8a38109999b427dd38180998763728a95291
Divyansh-ji <Divyanshtiwary01@gmail.com>
2026-01-26 13:28:49 +0530

    kafka 2

--- services/internals/kafka/producer.go
+++ services/internals/kafka/producer.go
@@ -1,1 +1,18 @@
 package kafka
+
+import (
+	"log"
+
+	"github.com/IBM/sarama"
+)
+
+func NewKafkaProducer(broker []string) sarama.SyncProducer {
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+
+	Producer, err := sarama.NewSyncProducer(broker, config)
+	if err != nil {
+		log.Println("error in creating kafka producer:", err)
+	}
+	return Producer
+}
